74f197a42ccdaa1796dbaa47ec5ec2eeffcd0440
Ilya Sanakin <finetnew@gmail.com>
2025-11-25 11:53:33 +0600

    Fix installation script

--- install-agent-go.sh
+++ install-agent-go.sh
@@ -1,6 +1,7 @@
 echo "Welcome to agent-go installation script"
+mkdir ~/.tmp
 cd ~/.tmp
-git clone https://github.com/finettt/agent-go ./agent-go
+git clone https://github.com/finettt/agent-go .
 cd ./agent-go
 make
 chown $USER:$USER ./agent-go
